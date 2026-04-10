5ba6edc7ebd649f5d8c5ff7b38a6fe994b56ebdd
armasstella <zs24013315@estudiantes.uv.mx>
2026-04-09 18:53:37 -0600

    feat(interfaces): add ILinkedOrganizationDAO class

---
+++ src/main/java/spp/businesslogic/interfaces/ILinkedOrganizationDAO.java
@@ -0,0 +1,7 @@
+package spp.businesslogic.interfaces;
+
+import spp.businesslogic.dto.LinkedOrganizationDTO;
+
+public interface ILinkedOrganizationDAO {
+    void addLinkedOrganization(LinkedOrganizationDTO linkedOrganization);
+}
