8fbf8617df68c04b9c393648dfc276a2eb4dac90
abdi <zS24013314@estudiantes.uv.mx>
2026-04-10 13:22:26 -0600

    feat(interfaces): implement signing methods in ICoordinatorDAO

--- src/main/java/spp/businesslogic/interfaces/ICoordinatorDAO.java
+++ src/main/java/spp/businesslogic/interfaces/ICoordinatorDAO.java
@@ -1,7 +1,14 @@
 package spp.businesslogic.interfaces;
 
-import spp.businesslogic.dto.InternDTO;
+import spp.businesslogic.dto.CoordinatorDTO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public interface ICoordinatorDAO {
-    void addCordinator(InternDTO internDTO);
+    void addCordinator(CoordinatorDTO coordinatorDTO);
+    int insertUser(Connection connection, CoordinatorDTO dto) throws SQLException;
+    void insertCoordinator(Connection connection, CoordinatorDTO dto, int userId) throws SQLException;
+    int getGeneratedKey(PreparedStatement preparedStatement) throws SQLException;
 }
