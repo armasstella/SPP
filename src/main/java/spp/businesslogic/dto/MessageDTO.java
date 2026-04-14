bece927917073adce067b934786e04b77c1ae6ff
abdi <zS24013314@estudiantes.uv.mx>
2026-04-14 17:37:24 -0600

    feat(dto): add MessageDTO class

---
+++ src/main/java/spp/businesslogic/dto/MessageDTO.java
@@ -0,0 +1,30 @@
+package spp.businesslogic.dto;
+
+import spp.businesslogic.enums.MesaggeStatus;
+
+public class MessageDTO {
+    private String content;
+    private MesaggeStatus messageStatus;
+
+    public MessageDTO() {
+
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setMessageStatus(MesaggeStatus messageStatus) {
+        this.messageStatus = messageStatus;
+    }
+
+    public MesaggeStatus getMessageStatus() {
+        return messageStatus;
+    }
+
+
+}
