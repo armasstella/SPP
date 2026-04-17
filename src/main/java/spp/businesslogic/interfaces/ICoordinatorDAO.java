9fae8af7a4b7086176233c4157404f456fbd3318
abdi <zS24013314@estudiantes.uv.mx>
2026-04-17 00:06:19 -0600

    refactor(interfaces): changed the contract of the methods

--- src/main/java/spp/businesslogic/interfaces/ICoordinatorDAO.java
+++ src/main/java/spp/businesslogic/interfaces/ICoordinatorDAO.java
@@ -2,6 +2,7 @@ package spp.businesslogic.interfaces;
 
 import spp.businesslogic.dto.CoordinatorDTO;
 import spp.businesslogic.exceptions.CoordinatorException;
+import spp.businesslogic.exceptions.DAOException;
 import spp.businesslogic.exceptions.DataAccessException;
 import spp.businesslogic.exceptions.LogicLayerException;
 
@@ -9,7 +10,6 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public interface ICoordinatorDAO {
-    void addCoordinator(CoordinatorDTO coordinatorDTO) throws CoordinatorException;
-    void insertCoordinator(CoordinatorDTO dto, int userId) throws LogicLayerException;
+    void addCoordinator(CoordinatorDTO coordinatorDTO) throws DAOException;
 
 }
