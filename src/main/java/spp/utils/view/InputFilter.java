77500314da4abb88ab028b6433d05a9246fa6010
abdi <zS24013314@estudiantes.uv.mx>
2026-05-31 11:29:06 -0600

    feat: add InputFilter class for validate and limit input fields

---
+++ src/main/java/spp/utils/view/InputFilter.java
@@ -0,0 +1,31 @@
+package spp.utils.view;
+
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.control.TextInputControl;
+
+import java.util.function.UnaryOperator;
+
+public final class InputFilter {
+
+    public static final String TEXT_PATTERN = "[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ¿?¡!'\"()/$#=%+\\-\\[\\]{}.,_ ]*";
+    public static final String NUMERIC_PATTERN = "\\d*";
+
+    private InputFilter() {
+    }
+
+    public static void applyFilter(TextInputControl inputControl, String regex, int maxLength) {
+        inputControl.setTextFormatter(new TextFormatter<>(buildFilter(regex, maxLength)));
+    }
+
+    private static UnaryOperator<TextFormatter.Change> buildFilter(String regex, int maxLength) {
+        return change -> {
+            TextFormatter.Change result = null;
+            String newText = change.getControlNewText();
+            if (newText.length() <= maxLength && newText.matches(regex)) {
+                result = change;
+            }
+            return result;
+        };
+    }
+}
