7dbe942fff791bd9751f9d3db7ab638fbb7e58ac
Nicole Armas <zs24013315@estudiantes.uv.mx>
2026-06-26 19:32:25 -0600

    Update NewLinkedOrganizationController.java

--- src/main/java/spp/presentation/controller/coordinator/NewLinkedOrganizationController.java
+++ src/main/java/spp/presentation/controller/coordinator/NewLinkedOrganizationController.java
@@ -1,6 +1,5 @@
 package spp.presentation.controller.coordinator;
 
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -9,13 +8,13 @@ import javafx.scene.control.TextField;
 import spp.businesslogic.dto.LinkedOrganizationDTO;
 import spp.businesslogic.exceptions.DAOException;
 import spp.businesslogic.dao.LinkedOrganizationDAO;
-import spp.utils.logger.AppLogger;
 import spp.utils.view.InputFilter;
 import spp.utils.view.StatusLabel;
+import spp.utils.view.ViewConstant;
 import spp.utils.view.ViewNavigator;
+
 import java.net.URL;
 import java.util.ResourceBundle;
-
 
 public class NewLinkedOrganizationController implements Initializable {
 
@@ -34,79 +33,116 @@ public class NewLinkedOrganizationController implements Initializable {
     }
 
     private void setUpFields() {
-        InputFilter.applyFilter(txtName, InputFilter.NAME_PATTERN, 40);
-        InputFilter.applyFilter(txtRfc, InputFilter.NAME_PATTERN, 13);
-        InputFilter.applyFilter(txtAddress, InputFilter.ALPHANUMERIC_PATTERN, 40);
-        InputFilter.applyFilter(txtFiscalAddress, InputFilter.ALPHANUMERIC_PATTERN, 40);
-        InputFilter.applyFilter(txtBusiness, InputFilter.ALPHANUMERIC_PATTERN, 100);
-        InputFilter.applyFilter(txtEmail, InputFilter.EMAIL_CHARS_PATTERN, 100);
-        InputFilter.applyFilter(txtPhoneNumber, InputFilter.NUMERIC_PATTERN, 10);
-
+        InputFilter.applyFormatFilter(txtName,
+                ViewConstant.PATTERN_ALPHANUMERIC, ViewConstant.MAX_LENGTH_TITLE);
+        InputFilter.applyFormatFilter(txtRfc,
+                ViewConstant.PATTERN_ALPHANUMERIC, ViewConstant.MAX_LENGTH_MORAL_RFC);
+        InputFilter.applyFormatFilter(txtAddress,
+                ViewConstant.PATTERN_ALPHANUMERIC, ViewConstant.MAX_LENGTH_ADDRESS);
+        InputFilter.applyFormatFilter(txtFiscalAddress,
+                ViewConstant.PATTERN_ALPHANUMERIC, ViewConstant.MAX_LENGTH_ADDRESS);
+        InputFilter.applyFormatFilter(txtBusiness,
+                ViewConstant.PATTERN_ALPHANUMERIC, ViewConstant.MAX_LENGTH_CATEGORY );
+        InputFilter.applyFormatFilter(txtEmail,
+                ViewConstant.PATTERN_EMAIL_CHARS, ViewConstant.MAX_LENGTH_EMAIL);
+        InputFilter.applyFormatFilter(txtPhoneNumber,
+                ViewConstant.PATTERN_NUMERIC, ViewConstant.MAX_LENGTH_PHONE);
     }
 
-    @FXML
     private void setAllLinkedOrganization(LinkedOrganizationDTO linkedOrganizationDTO) {
-        linkedOrganizationDTO.setName(txtName.getText().trim());
-        linkedOrganizationDTO.setRfc(txtRfc.getText().trim());
-        linkedOrganizationDTO.setAddress(txtAddress.getText().trim());
-        linkedOrganizationDTO.setFiscalAddress(txtFiscalAddress.getText().trim());
-        linkedOrganizationDTO.setBusiness(txtBusiness.getText().trim());
-        linkedOrganizationDTO.setPhoneNumber(txtPhoneNumber.getText().trim());
-        linkedOrganizationDTO.setEmail(txtEmail.getText().trim());
-
+        linkedOrganizationDTO.setName(txtName.getText());
+        linkedOrganizationDTO.setRfc(txtRfc.getText());
+        linkedOrganizationDTO.setAddress(txtAddress.getText());
+        linkedOrganizationDTO.setFiscalAddress(txtFiscalAddress.getText());
+        linkedOrganizationDTO.setBusiness(txtBusiness.getText());
+        linkedOrganizationDTO.setPhoneNumber(txtPhoneNumber.getText());
+        linkedOrganizationDTO.setEmail(txtEmail.getText());
     }
+
+    private boolean hasEmptyFields() {
+        boolean emptyFields = false;
+
+        if (txtName.getText().isBlank() ||
+                txtRfc.getText().isBlank() ||
+                txtAddress.getText().isBlank() ||
+                txtFiscalAddress.getText().isBlank() ||
+                txtBusiness.getText().isBlank() ||
+                txtPhoneNumber.getText().isBlank() ||
+                txtEmail.getText().isBlank()) {
+
+            emptyFields = true;
+        }
+
+        return emptyFields;
+    }
+
+    private boolean hasValidMinimumLengths() {
+        boolean validLengths = false;
+
+        boolean validName = InputFilter.hasMinimumLength(txtName,
+                ViewConstant.MIN_LENGTH_NAME);
+        boolean validRfc = InputFilter.hasMinimumLength(txtRfc,
+                ViewConstant.MIN_LENGTH_MORAL_RFC);
+        boolean validAddress = InputFilter.hasMinimumLength(txtAddress,
+                ViewConstant.MIN_LENGTH_ADDRESS);
+        boolean validFiscalAddress = InputFilter.hasMinimumLength(txtFiscalAddress,
+                ViewConstant.MIN_LENGTH_ADDRESS);
+        boolean validBusiness = InputFilter.hasMinimumLength(txtBusiness,
+                ViewConstant.MIN_LENGTH_CATEGORY);
+        boolean validPhone = InputFilter.hasMinimumLength(txtPhoneNumber,
+                ViewConstant.MIN_LENGTH_PHONE);
+
+        if (validName && validRfc && validAddress && validFiscalAddress && validBusiness && validPhone) {
+            validLengths = true;
+        }
+
+        return validLengths;
+    }
+
+    private boolean areValidFields() {
+        boolean validFields = false;
+
+        if (hasEmptyFields()) {
+            StatusLabel.showError(lblStatus, "Completa todos los campos obligatorios.");
+        } else {
+            if (hasValidMinimumLengths()) {
+                validFields = true;
+            } else {
+                StatusLabel.showError(lblStatus, "La longitud de los campos debe cumplir con el mínimo de caracteres.");
+            }
+        }
+
+        return validFields;
+    }
+
 
     @FXML
     private void saveLinkedOrganization(ActionEvent event) {
-        if (validateRegistrationInputs()) {
-            return;
-        }
+        if (areValidFields()) {
+            LinkedOrganizationDTO linkedOrganizationDTO = new LinkedOrganizationDTO();
+            setAllLinkedOrganization(linkedOrganizationDTO);
 
-        LinkedOrganizationDTO linkedOrganizationDTO = new LinkedOrganizationDTO();
-        setAllLinkedOrganization(linkedOrganizationDTO);
-
-        if (linkedOrganizationDTO.isValid()) {
-            LinkedOrganizationDAO linkedOrganizationDAO = new LinkedOrganizationDAO();
-            try {
-                if (linkedOrganizationDAO.registerLinkedOrganization(linkedOrganizationDTO)) {
-                    StatusLabel.showSuccess(lblStatus, "Organización registrada correctamente.");
-                    clearInputFields();
+            if (linkedOrganizationDTO.isValid()) {
+                LinkedOrganizationDAO linkedOrganizationDAO = new LinkedOrganizationDAO();
+                try {
+                    if (linkedOrganizationDAO.registerLinkedOrganization(linkedOrganizationDTO)) {
+                        StatusLabel.showSuccess(lblStatus, "Organización registrada correctamente.");
+                        clearInputFields();
+                    }
+                } catch (DAOException e) {
+                    StatusLabel.showError(lblStatus, "No se pudo registrar la organización.");
                 }
-            } catch (DAOException e) {
-                AppLogger.logError(e);
-                StatusLabel.showError(lblStatus, e.getMessage());
+            } else {
+                String errorMessages = String.join("\n• ", linkedOrganizationDTO.getErrors());
+                StatusLabel.showError(lblStatus, "Corrige los siguientes formatos:\n• " + errorMessages);
             }
-        } else {
-            String errorMessages = String.join("\n. ", linkedOrganizationDTO.getErrors());
-            StatusLabel.showError(lblStatus, "Corrige los siguientes formatos:\n. " + errorMessages);
         }
-
     }
 
     @FXML
     private void cancel(ActionEvent event) {
         ViewNavigator.loadView("/spp/presentation/view/coordinator/CoordinatorMenuView.fxml",
                 "Menú Coordinador", event);
-
-    }
-
-    private boolean validateRegistrationInputs() {
-        boolean validFields = false;
-
-        if (txtName.getText().isBlank() ||
-                txtName.getText().isBlank() ||
-                txtRfc.getText().isBlank() ||
-                txtAddress.getText().isBlank() ||
-                txtFiscalAddress.getText().isBlank() ||
-                txtBusiness.getText().isBlank() ||
-                txtPhoneNumber.getText().isBlank() ||
-                txtEmail.getText().isBlank()){
-            StatusLabel.showError(lblStatus, "Completa todos los campos obligatorios.");
-            validFields = true;
-        }
-
-        return validFields;
-
     }
 
     private void clearInputFields() {
@@ -117,7 +153,5 @@ public class NewLinkedOrganizationController implements Initializable {
         txtBusiness.clear();
         txtPhoneNumber.clear();
         txtEmail.clear();
-
     }
-
 }
