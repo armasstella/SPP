e5d2caedf8501dd80308908bf9352d3e2db3cd45
Nicole Armas <zs24013315@estudiantes.uv.mx>
2026-06-26 20:51:22 -0600

    Update UploadDocumentsController.java

--- src/main/java/spp/presentation/controller/intern/UploadDocumentsController.java
+++ src/main/java/spp/presentation/controller/intern/UploadDocumentsController.java
@@ -14,7 +14,6 @@ import spp.businesslogic.exceptions.DAOException;
 import spp.businesslogic.dao.InitialDocumentDAO;
 import spp.businesslogic.dao.InternDAO;
 import spp.utils.file.FileUtils;
-import spp.utils.logger.AppLogger;
 import spp.utils.view.FileChooserUtil;
 import spp.utils.view.StatusLabel;
 import spp.utils.view.ViewNavigator;
@@ -45,28 +44,25 @@ public class UploadDocumentsController {
     private void uploadClassSchedule(ActionEvent event) {
         if (existsClassSchedule()) {
             StatusLabel.showError(lblStatus, "Ya ha subido el horario.\nComuníquese con el coordinador.");
-            return;
+        } else {
+            if (selectFile(event, "Seleccionar horario")) {
+                initialDocumentDTO.setDocumentType(String.valueOf(DocumentType.CLASS_SCHEDULE));
+                currentFolder = "./documents/schedules/";
+                currentPrefix = "schedule";
+            }   
         }
-
-        if (selectFile(event, "Seleccionar horario")) {
-            initialDocumentDTO.setDocumentType(String.valueOf(DocumentType.CLASS_SCHEDULE));
-            currentFolder = "./documents/schedules/";
-            currentPrefix = "schedule";
-        }
-
     }
 
     @FXML
     private void uploadActivitiesSchedule(ActionEvent event) {
         if (existsActivitiesSchedule()) {
             StatusLabel.showError(lblStatus, "Ya ha subido la calendarización de actividades.\nComuníquese con el coordinador.");
-            return;
-        }
-
-        if (selectFile(event, "Seleccionar calendarización de actividades")) {
-            initialDocumentDTO.setDocumentType(String.valueOf(DocumentType.ACTIVITIES_SCHEDULE));
-            currentFolder = "./documents/activities/";
-            currentPrefix = "activity";
+        } else {
+            if (selectFile(event, "Seleccionar calendarización de actividades")) {
+                initialDocumentDTO.setDocumentType(String.valueOf(DocumentType.ACTIVITIES_SCHEDULE));
+                currentFolder = "./documents/activities/";
+                currentPrefix = "activity";
+            }
         }
 
     }
@@ -75,75 +71,65 @@ public class UploadDocumentsController {
     private void uploadPSP(ActionEvent event) {
         if (existsPSP()) {
             StatusLabel.showError(lblStatus, "Ya ha subido la bitácora PSP.\nComuníquese con el coordinador.");
-            return;
+        } else {
+            if (selectFile(event, "Seleccionar bitácora PSP")) {
+                initialDocumentDTO.setDocumentType(String.valueOf(DocumentType.PSP));
+                currentFolder = "./documents/psp/";
+                currentPrefix = "psp";
+            }
         }
-
-        if (selectFile(event, "Seleccionar bitácora PSP")) {
-            initialDocumentDTO.setDocumentType(String.valueOf(DocumentType.PSP));
-            currentFolder = "./documents/psp/";
-            currentPrefix = "psp";
-        }
-
     }
 
     @FXML
     private void uploadPartialReport(ActionEvent event) {
         if (existsPartialReport()) {
             StatusLabel.showError(lblStatus, "Ya ha subido el reporte parcial.\nComuníquese con el coordinador.");
-            return;
+        } else {
+            if (selectFile(event, "Seleccionar reporte parcial")) {
+                initialDocumentDTO.setDocumentType(String.valueOf(DocumentType.INDICATOR_REPORT));
+                currentFolder = "./documents/reports/partial/";
+                currentPrefix = "partial";
+            }
         }
-
-        if (selectFile(event, "Seleccionar reporte parcial")) {
-            initialDocumentDTO.setDocumentType(String.valueOf(DocumentType.INDICATOR_REPORT));
-            currentFolder = "./documents/reports/partial/";
-            currentPrefix = "partial";
-        }
-
     }
 
     @FXML
     private void uploadSelfEvaluation(ActionEvent event) {
         if (existsSelfEvaluation()) {
             StatusLabel.showError(lblStatus, "Ya ha subido la autoevaluación.\nComuníquese con el coordinador.");
-            return;
+        } else {
+            if (selectFile(event, "Seleccionar autoevaluación")) {
+                initialDocumentDTO.setDocumentType(String.valueOf(DocumentType.SELF_EVALUATION));
+                currentFolder = "./documents/evaluations/self/";
+                currentPrefix = "self_evaluation";
+            }
         }
-
-        if (selectFile(event, "Seleccionar autoevaluación")) {
-            initialDocumentDTO.setDocumentType(String.valueOf(DocumentType.SELF_EVALUATION));
-            currentFolder = "./documents/evaluations/self/";
-            currentPrefix = "self_evaluation";
-        }
-
     }
 
     @FXML
     private void uploadEvaluationLinkedOrganization(ActionEvent event) {
         if (existsEvaluationLinkedOrganization()) {
             StatusLabel.showError(lblStatus, "Ya ha subido la evaluación de la organización.\nComuníquese con el coordinador.");
-            return;
+        } else {
+            if (selectFile(event, "Seleccionar evaluación de organización vinculada")) {
+                initialDocumentDTO.setDocumentType(String.valueOf(DocumentType.EVALUATION_LINKED_ORGANIZATION));
+                currentFolder = "./documents/evaluations/organization/";
+                currentPrefix = "organization_evaluation";
+            }
         }
-
-        if (selectFile(event, "Seleccionar evaluación de organización vinculada")) {
-            initialDocumentDTO.setDocumentType(String.valueOf(DocumentType.EVALUATION_LINKED_ORGANIZATION));
-            currentFolder = "./documents/evaluations/organization/";
-            currentPrefix = "organization_evaluation";
-        }
-
     }
 
     @FXML
     private void uploadFinalReport(ActionEvent event) {
         if (existsFinalReport()) {
             StatusLabel.showError(lblStatus, "Ya ha subido el reporte final.\nComuníquese con el coordinador.");
-            return;
+        } else {
+            if (selectFile(event, "Seleccionar reporte final")) {
+                initialDocumentDTO.setDocumentType(String.valueOf(DocumentType.FINAL_REPORT));
+                currentFolder = "./documents/reports/final/";
+                currentPrefix = "final";
+            }
         }
-
-        if (selectFile(event, "Seleccionar reporte final")) {
-            initialDocumentDTO.setDocumentType(String.valueOf(DocumentType.FINAL_REPORT));
-            currentFolder = "./documents/reports/final/";
-            currentPrefix = "final";
-        }
-
     }
 
     private boolean existsClassSchedule() {
@@ -151,7 +137,6 @@ public class UploadDocumentsController {
         try {
             exists = initialDocumentDAO.hasClassScheduleByInternEmail(ActiveSessionDTO.get().getEmail());
         } catch (DAOException e) {
-            AppLogger.logError(e);
             StatusLabel.showError(lblStatus, "Error al buscar el horario.");
         }
         return exists;
@@ -163,7 +148,6 @@ public class UploadDocumentsController {
         try {
             exists = initialDocumentDAO.hasActivitiesScheduleByInternEmail(ActiveSessionDTO.get().getEmail());
         } catch (DAOException e) {
-            AppLogger.logError(e);
             StatusLabel.showError(lblStatus, "Error al buscar la calendarización.");
         }
         return exists;
@@ -175,7 +159,6 @@ public class UploadDocumentsController {
         try {
             exists = initialDocumentDAO.hasPSPByInternEmail(ActiveSessionDTO.get().getEmail());
         } catch (DAOException e) {
-            AppLogger.logError(e);
             StatusLabel.showError(lblStatus, "Error al buscar la bitácora PSP.");
         }
         return exists;
@@ -187,7 +170,6 @@ public class UploadDocumentsController {
         try {
             exists = initialDocumentDAO.hasPartialReportByInternEmail(ActiveSessionDTO.get().getEmail());
         } catch (DAOException e) {
-            AppLogger.logError(e);
             StatusLabel.showError(lblStatus, "Error al buscar el reporte parcial.");
         }
         return exists;
@@ -199,7 +181,6 @@ public class UploadDocumentsController {
         try {
             exists = initialDocumentDAO.hasSelfEvaluationByInternEmail(ActiveSessionDTO.get().getEmail());
         } catch (DAOException e) {
-            AppLogger.logError(e);
             StatusLabel.showError(lblStatus, "Error al buscar la autoevaluación.");
         }
         return exists;
@@ -211,7 +192,6 @@ public class UploadDocumentsController {
         try {
             exists = initialDocumentDAO.hasEvaluationLinkedOrganizationByInternEmail(ActiveSessionDTO.get().getEmail());
         } catch (DAOException e) {
-            AppLogger.logError(e);
             StatusLabel.showError(lblStatus, "Error al buscar la evaluación de la organización.");
         }
         return exists;
@@ -224,7 +204,6 @@ public class UploadDocumentsController {
         try {
             exist = finalReportDAO.hasFinalReportByInternEmail(ActiveSessionDTO.get().getEmail());
         } catch (DAOException e) {
-            AppLogger.logError(e);
             StatusLabel.showError(lblStatus, "Error al buscar el reporte final.");
         }
         return exist;
@@ -234,59 +213,55 @@ public class UploadDocumentsController {
     private boolean selectFile(ActionEvent event, String dialogTitle) {
         Window window = ((Node) event.getSource()).getScene().getWindow();
         File file = FileChooserUtil.selectSingleFile(window, dialogTitle);
+        boolean isFileSelected = false;
 
         if (file != null) {
             selectedDocument = file;
             lblSelectedDocument.setText("Archivo seleccionado: " + selectedDocument.getName());
             StatusLabel.showSuccess(lblStatus, "Archivo listo para subir.");
-            return true;
+            isFileSelected = true;
         }
-        return false;
+        return isFileSelected;
     }
 
     @FXML
     private void confirm() {
-        if (validateEmptyInputs()) {
-            return;
-        }
-
-        if (setFileMetadata()) {
-            if (!saveDataDocument()) {
-                return;
+        if (!hasInvalidInputs()) {
+            if (setFileMetadata()) {
+                if (saveDataDocument()) {
+                    StatusLabel.showSuccess(lblStatus, "Archivo cargado exitosamente");
+                    lblSelectedDocument.setText("Ningún archivo seleccionado");
+                    selectedDocument = null;
+                    currentFolder = null;
+                    currentPrefix = null;
+                }
             }
-        } else {
-            return;
         }
-
-        StatusLabel.showSuccess(lblStatus, "Archivo cargado exitosamente");
-        lblSelectedDocument.setText("Ningún archivo seleccionado");
-        selectedDocument = null;
-        currentFolder = null;
-        currentPrefix = null;
     }
 
-    private boolean validateEmptyInputs() {
+    private boolean hasInvalidInputs() {
+        boolean isValidFile = false;
         if (selectedDocument == null) {
             StatusLabel.showError(lblStatus, "No se ha elegido un archivo.");
-            return true;
+        } else {
+            String extension = FileUtils.getExtension(selectedDocument.getName());
+
+            if (!FileUtils.ALLOWED_EXTENSIONS.contains(extension)) {
+                StatusLabel.showError(lblStatus, "Formato invalido. Solo se acepta PDF o DOCX.");
+            } else {
+                if (selectedDocument.length() == 0) {
+                    StatusLabel.showError(lblStatus, "El documento está vacío y no puede guardarse.");
+                } else {
+                    if (selectedDocument.length() > FileUtils.MAX_BYTES) {
+                        StatusLabel.showError(lblStatus, "El tamaño de archivo excede el permitido.");
+                    } else {
+                        isValidFile = true;
+                    }
+                }
+            }
         }
 
-        String extension = FileUtils.getExtension(selectedDocument.getName());
-
-        if (!FileUtils.ALLOWED_EXTENSIONS.contains(extension)) {
-            StatusLabel.showError(lblStatus, "Formato invalido. Solo se acepta PDF o DOCX.");
-            return true;
-        }
-        if (selectedDocument.length() == 0) {
-            StatusLabel.showError(lblStatus, "El documento está vacío y no puede guardarse.");
-            return true;
-        }
-        if (selectedDocument.length() > FileUtils.MAX_BYTES) {
-            StatusLabel.showError(lblStatus, "El tamaño de archivo excede el permitido.");
-            return true;
-        }
-
-        return false;
+        return isValidFile;
     }
 
     private boolean setFileMetadata() {
@@ -307,7 +282,6 @@ public class UploadDocumentsController {
             saveStatus = true;
 
         } catch (IOException | DAOException e) {
-            AppLogger.logError(e);
             StatusLabel.showError(lblStatus, "No se puede guardar el archivo. Intente de nuevo.");
         }
 
@@ -320,7 +294,6 @@ public class UploadDocumentsController {
             String studentNumber = internDAO.findActiveStudentNumberByEmail(ActiveSessionDTO.get().getEmail());
             success = initialDocumentDAO.saveDocument(studentNumber, initialDocumentDTO);
         } catch (DAOException e) {
-            AppLogger.logError(e);
             StatusLabel.showError(lblStatus, "Error al guardar documento");
         }
 
