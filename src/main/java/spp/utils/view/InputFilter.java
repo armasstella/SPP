e41ea0cfdc57da6a9a6fd13807dd9387443f086e
Nicole Armas <zs24013315@estudiantes.uv.mx>
2026-06-26 20:59:27 -0600

    Update InputFilter.java

--- src/main/java/spp/utils/view/InputFilter.java
+++ src/main/java/spp/utils/view/InputFilter.java
@@ -2,35 +2,33 @@ package spp.utils.view;
 
 import javafx.scene.control.TextFormatter;
 import javafx.scene.control.TextInputControl;
-import java.util.function.UnaryOperator;
 import java.util.regex.Pattern;
 
 public final class InputFilter {
-
-    public static final Pattern NAME_PATTERN = Pattern.compile("[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\\s]*");
-    public static final Pattern NUMERIC_PATTERN = Pattern.compile("\\d*");
-    public static final Pattern EMAIL_CHARS_PATTERN = Pattern.compile("[a-zA-Z0-9@._\\-]*");
-    public static final Pattern ALPHANUMERIC_PATTERN = Pattern.compile("[a-zA-Z0-9áéíóúÁÉÍÓÚüÜñÑ\\s.,#\\-]*");
-    public static final Pattern PASSWORD_PATTERN = Pattern.compile("[a-zA-Z0-9ñÑ.*%!-]*");
 
     private InputFilter() {
 
     }
 
-    public static void applyFilter(TextInputControl inputControl, Pattern regex, int maxLength) {
-        inputControl.setTextFormatter(new TextFormatter<>(buildFilter(regex, maxLength)));
+    public static void applyFormatFilter(TextInputControl inputField, Pattern allowedPattern, int maximumLength) {
+        FormatFilter customFilter = new FormatFilter(allowedPattern, maximumLength);
+        TextFormatter<String> textFormatter = new TextFormatter<>(customFilter);
 
+        inputField.setTextFormatter(textFormatter);
     }
 
-    private static UnaryOperator<TextFormatter.Change> buildFilter(Pattern regex, int maxLength) {
-        return change -> {
-            String newText = change.getControlNewText();
-            if (newText.length() <= maxLength && regex.matcher(newText).matches()) {
-                return change;
-            }
-            return null;
-        };
+    public static boolean hasMinimumLength(TextInputControl inputField, int minimumLength) {
+        boolean meetsMinimum;
+        String currentText = inputField.getText();
 
+        if (currentText != null) {
+            int currentTextLength = currentText.trim().length();
+
+            meetsMinimum = currentTextLength >= minimumLength;
+        } else {
+            meetsMinimum = false;
+        }
+
+        return meetsMinimum;
     }
-
 }
