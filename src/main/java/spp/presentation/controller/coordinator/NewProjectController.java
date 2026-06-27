818f9d5c14723bba61795334ceed1e4f2fa7b4dc
Nicole Armas <zs24013315@estudiantes.uv.mx>
2026-06-26 19:33:45 -0600

    Update NewProjectController.java

--- src/main/java/spp/presentation/controller/coordinator/NewProjectController.java
+++ src/main/java/spp/presentation/controller/coordinator/NewProjectController.java
@@ -1,6 +1,5 @@
 package spp.presentation.controller.coordinator;
 
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -16,14 +15,14 @@ import spp.businesslogic.dto.ProjectDTO;
 import spp.businesslogic.dto.ProjectManagerDTO;
 import spp.businesslogic.exceptions.DAOException;
 import spp.businesslogic.dao.ProjectDAO;
-import spp.utils.logger.AppLogger;
 import spp.utils.view.InputFilter;
 import spp.utils.view.StatusLabel;
+import spp.utils.view.ViewConstant;
 import spp.utils.view.ViewNavigator;
+
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
-
 
 public class NewProjectController implements Initializable {
 
@@ -39,101 +38,117 @@ public class NewProjectController implements Initializable {
         setUpFields();
         loadLinkedOrganizationInComboBox();
         loadProjectManagersInComboBox();
-
-    }
-
-    private void loadProjectManagersInComboBox() {
-        try {
-            ProjectManagerDAO projectManagerDAO = new ProjectManagerDAO();
-            List<ProjectManagerDTO> projectManagerList =
-                    projectManagerDAO.getActiveProjectManagers();
-            ObservableList<ProjectManagerDTO> projectManagerObservableList =
-                    FXCollections.observableArrayList(projectManagerList);
-            cmbProjectManager.setItems(projectManagerObservableList);
-        } catch (DAOException e) {
-            AppLogger.logError(e);
-            StatusLabel.showError(lblStatus, "Error al cargar encargados de proyecto");
-        }
-
-    }
-
-    private void loadLinkedOrganizationInComboBox() {
-        try {
-            LinkedOrganizationDAO linkedOrganizationDAO = new LinkedOrganizationDAO();
-            List<LinkedOrganizationDTO> linkedOrganizationList =
-                    linkedOrganizationDAO.findActiveLinkedOrganizationsIdentifiers();
-            ObservableList<LinkedOrganizationDTO> linkedOrganizationObservableList =
-                    FXCollections.observableArrayList(linkedOrganizationList);
-            cmbLinkedOrganization.setItems(linkedOrganizationObservableList);
-        } catch (DAOException e) {
-            AppLogger.logError(e);
-            StatusLabel.showError(lblStatus, "Error al cargar organizaciones vinculadas");
-        }
-
     }
 
     private void setUpFields() {
-        InputFilter.applyFilter(txtName, InputFilter.NAME_PATTERN, 40);
-        InputFilter.applyFilter(txtDescription, InputFilter.ALPHANUMERIC_PATTERN, 40);
-        InputFilter.applyFilter(txtPlacesAvailable, InputFilter.NUMERIC_PATTERN, 2);
-
+        InputFilter.applyFormatFilter(txtName,
+                ViewConstant.PATTERN_ALPHANUMERIC, ViewConstant.MAX_LENGTH_TITLE);
+        InputFilter.applyFormatFilter(txtDescription,
+                ViewConstant.PATTERN_ALPHANUMERIC, ViewConstant.MAX_LENGTH_DESCRIPTION);
+        InputFilter.applyFormatFilter(txtPlacesAvailable,
+                ViewConstant.PATTERN_NUMERIC, ViewConstant.MAX_LENGTH_CAPACITY);
     }
 
     private void setAllProject(ProjectDTO projectDTO) {
-        ProjectManagerDTO projectManagerDTO = new ProjectManagerDTO();
-        LinkedOrganizationDTO linkedOrganizationDTO = new LinkedOrganizationDTO();
-        projectDTO.setName(txtName.getText().trim());
-        projectDTO.setDescription(txtDescription.getText().trim());
-        projectDTO.setPlacesAvailable(Integer.parseInt(txtPlacesAvailable.getText().trim()));
-        projectManagerDTO.setId(cmbProjectManager.getValue().getId());
-        projectDTO.setProjectManagerDTO(projectManagerDTO);
-        linkedOrganizationDTO.setId(cmbLinkedOrganization.getValue().getId());
-        projectDTO.setLinkedOrganizationDTO(linkedOrganizationDTO);
-
+        projectDTO.setName(txtName.getText());
+        projectDTO.setDescription(txtDescription.getText());
+        projectDTO.setPlacesAvailable(Integer.parseInt(txtPlacesAvailable.getText()));
+        projectDTO.setProjectManagerDTO(cmbProjectManager.getValue());
+        projectDTO.setLinkedOrganizationDTO(cmbLinkedOrganization.getValue());
     }
 
-    @FXML
-    private void saveProject(ActionEvent event) {
-        if (validateRegistrationInputs()) {
-            return;
-        }
-        ProjectDAO projectDAO = new ProjectDAO();
-        ProjectDTO projectDTO = new ProjectDTO();
-        setAllProject(projectDTO);
-
-        try {
-            if (projectDAO.registerProject(projectDTO)) {
-                StatusLabel.showSuccess(lblStatus, "Proyecto registrado correctamente.");
-                clearInputFields();
-            }
-
-        } catch (DAOException e) {
-            AppLogger.logError(e);
-            StatusLabel.showError(lblStatus, e.getMessage());
-        }
-
-    }
-
-    @FXML
-    private void cancel(ActionEvent event) {
-        ViewNavigator.loadView("/spp/presentation/view/coordinator/CoordinatorMenuView.fxml",
-                "Cancelar", event);
-
-    }
-
-    private boolean validateRegistrationInputs() {
+    private boolean hasEmptyFields() {
         boolean emptyFields = false;
+
         if (txtName.getText().isBlank() ||
                 txtDescription.getText().isBlank() ||
                 txtPlacesAvailable.getText().isBlank() ||
                 cmbProjectManager.getValue() == null ||
                 cmbLinkedOrganization.getValue() == null) {
-            StatusLabel.showError(lblStatus, "Completa todos los campos obligatorios.");
+
             emptyFields = true;
         }
 
         return emptyFields;
+    }
 
+    private boolean hasValidMinimumLengths() {
+        boolean validLengths = false;
+
+        boolean validName = InputFilter.hasMinimumLength(txtName, ViewConstant.
+                MIN_LENGTH_NAME);
+
+        if (validName) {
+            validLengths = true;
+        }
+
+        return validLengths;
+    }
+
+    private boolean areValidFields() {
+        boolean validFields = false;
+
+        if (hasEmptyFields()) {
+            StatusLabel.showError(lblStatus, "Completa todos los campos obligatorios.");
+        } else {
+            if (hasValidMinimumLengths()) {
+                validFields = true;
+            } else {
+                StatusLabel.showError(lblStatus, "La longitud de los campos debe cumplir con el mínimo de caracteres.");
+            }
+        }
+
+        return validFields;
+    }
+
+    @FXML
+    private void saveProject(ActionEvent event) {
+        if (areValidFields()) {
+            ProjectDTO projectDTO = new ProjectDTO();
+            setAllProject(projectDTO);
+
+            ProjectDAO projectDAO = new ProjectDAO();
+            try {
+                if (projectDAO.registerProject(projectDTO)) {
+                    StatusLabel.showSuccess(lblStatus, "Proyecto registrado correctamente.");
+                    clearInputFields();
+                }
+            } catch (DAOException e) {
+                StatusLabel.showError(lblStatus, "No se pudo registrar el proyecto");
+            }
+        }
+    }
+
+    @FXML
+    private void cancel(ActionEvent event) {
+        ViewNavigator.loadView("/spp/presentation/view/coordinator/CoordinatorMenuView.fxml",
+                "Menú Coordinador", event);
+    }
+
+    private void loadProjectManagersInComboBox() {
+        try {
+            ProjectManagerDAO projectManagerDAO = new ProjectManagerDAO();
+            List<ProjectManagerDTO> projectManagerList = projectManagerDAO.getActiveProjectManagers();
+            ObservableList<ProjectManagerDTO> projectManagerObservableList = FXCollections.observableArrayList(projectManagerList);
+
+            cmbProjectManager.setItems(projectManagerObservableList);
+
+        } catch (DAOException e) {
+            StatusLabel.showError(lblStatus, "Error al cargar la lista de encargados de proyecto.");
+        }
+    }
+
+    private void loadLinkedOrganizationInComboBox() {
+        try {
+            LinkedOrganizationDAO linkedOrganizationDAO = new LinkedOrganizationDAO();
+            List<LinkedOrganizationDTO> linkedOrganizationList = linkedOrganizationDAO.findActiveLinkedOrganizationsIdentifiers();
+            ObservableList<LinkedOrganizationDTO> linkedOrganizationObservableList = FXCollections.observableArrayList(linkedOrganizationList);
+
+            cmbLinkedOrganization.setItems(linkedOrganizationObservableList);
+
+        } catch (DAOException e) {
+            StatusLabel.showError(lblStatus, "Error al cargar la lista de organizaciones vinculadas.");
+        }
     }
 
     private void clearInputFields() {
@@ -142,7 +157,5 @@ public class NewProjectController implements Initializable {
         txtPlacesAvailable.clear();
         cmbProjectManager.setValue(null);
         cmbLinkedOrganization.setValue(null);
-
     }
-
 }
